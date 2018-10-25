d9d600dbc3381b4167f6d4ca5a5384a62d34e575
vadvag <vadvag@gmail.com>
2018-10-25 13:11:33 +0300

    add command line interface

---
+++ 1c7la.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.5
+import argparse
+
+
+def getparser():
+	parser =  argparse.ArgumentParser(description='Comand line argument parser')
+	parser.add_argument('filename', action='store', help='Name of 1cv77 log file')
+	return parser.parse_args()
+
+
+try:
+	args = getparser()
+	print(args.filename)
+except:
+	print('Some Error')
