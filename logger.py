33fba4d3ad6e63a3f7493134b6f045e1f667eba8
vadvag <30406773+vadvag@users.noreply.github.com>
2018-10-26 10:14:20 +0300

    Deleted some prints

--- logger.py
+++ logger.py
@@ -2,15 +2,12 @@ class Logger():
     data = ""
     
     def read(self,  filename):
-        print(2)
         with open(filename,  'rt', encoding='cp1251') as f:
-            #fn = f.readline()
             for line in f:
-                print(1)
-                self.data = line
-                print(self.data)
-            
-        print('read filename {}'.format(filename))
+                self.data = line #debug info
+                print(self.data) #debug info. print last line.
+        
+        #print('read filename {}'.format(filename))
         
     def __init__(self):
         ...
