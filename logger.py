d85f2fc08c7c0d256e94408bb9845bd111e627dd
vadvag <vadvag@gmail.com>
2018-10-30 10:22:03 +0200

    issure #8 I added a function convertdatetime() 	changed:       logger.py

--- logger.py
+++ logger.py
@@ -1,5 +1,17 @@
+import datetime
+
 class Logger():
     data = ""
+    
+    def convertdatetime(self, tdate,  ttime):
+        _tm = ttime.split(":")
+        _rawdata = datetime.datetime(int(tdate[:4])
+                                    , int(tdate[4:6])
+                                    , int(tdate[6:8])
+                                    , int(_tm[0]) 
+                                    , int(_tm[1])
+                                    , int(_tm[2]))
+        return _rawdata
     
     def read(self,  filename):
         with open(filename,  'rt', encoding='cp1251') as f:
@@ -17,6 +29,6 @@ class Logger():
 
 if __name__ == '__main__':
     logger = Logger()
-    logger.read("/home/pavlo72/XPCommon/1cv7.mlg")
-    print(logger.data)
-    
+    #logger.read("/home/pavlo72/XPCommon/1cv7.mlg")
+    #print(logger.data)
+    print(logger.convertdatetime("20181005", "22:00:00"))
