76c088f9e5743230f46ae1caafd6b5c50e6082ea
vadvag <vadvag@gmail.com>
2018-11-10 21:10:18 +0200

    Example charts 	new file:    plot.py

---
+++ plot.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+s = [15, 32, 88, 59, 44,  -32, 16]
+x = range(len(s))
+ax = plt.gca()
+ax.bar(x, s, align='edge') # align='edge' - выравнивание по границе, а не по центру
+ax.set_xticks(x)
+ax.set_xticklabels(('8:30', '9:00', '10:30', '12:00',  '14:30', '16:00', '17:30'))
+plt.show()
